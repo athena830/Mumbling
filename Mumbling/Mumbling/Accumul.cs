a33341b82f00edf89ca1e1ecd56cfec97ae92177
Athena Chang <athena830@gmail.com>
2017-07-24 11:31:32 +0800

    First Commit

---
+++ Mumbling/Mumbling/Accumul.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Summary description for Accumul
+/// </summary>
+public class Accumul
+{
+    public static String Accum(string source)
+    {
+        return string.Join("-", TransformCharacters(source.ToArray()));       
+    }
+
+    private static IEnumerable<string> TransformCharacters(char[] sourceCharArray)
+    {
+        for (int i = 0; i < sourceCharArray.Length; i++)
+        {
+            var currentChar = sourceCharArray[i];
+            yield return UpperFirstCharacter(currentChar) +
+                         RepeatLowerCharacter(i, currentChar);
+        }
+    }
+
+    private static string UpperFirstCharacter(char currentChar)
+    {
+        return currentChar.ToString().ToUpper();
+    }
+
+    private static string RepeatLowerCharacter(int times, char currentChar)
+    {
+        return string.Concat(Enumerable.Repeat(currentChar.ToString().ToLower(), times));
+    }
+}
---
+++ Mumbling/Mumbling/AccumulTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+/// <summary>
+/// Summary description for AccumulTests
+/// </summary>
+[TestFixture]
+public class AccumulTests
+{
+    private static void testing(string actual, string expected)
+    {
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public static void test1()
+    {
+        testing(Accumul.Accum("A"), "A");
+        testing(Accumul.Accum("a"), "A");
+        testing(Accumul.Accum("ab"), "A-Bb");
+        testing(Accumul.Accum("AB"), "A-Bb");
+        testing(Accumul.Accum("ABC"), "A-Bb-Ccc");
+    }
+
+    [Test]
+    public void Empty()
+    {
+        testing(Accumul.Accum(""), "");
+    }
+}

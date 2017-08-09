22bdf6e1c79e6b5a03aa249964a106607fbff278
athena830 <athena830@gmail.com>
2017-08-09 14:05:04 +0800

    Commit original code

--- Mumbling/Mumbling/Accumul.cs
+++ Mumbling/Mumbling/Accumul.cs
@@ -9,26 +9,17 @@ public class Accumul
 {
     public static String Accum(string source)
     {
-        return string.Join("-", TransformCharacters(source.ToArray()));       
-    }
-
-    private static IEnumerable<string> TransformCharacters(char[] sourceCharArray)
-    {
-        for (int i = 0; i < sourceCharArray.Length; i++)
+        string str = string.Empty;
+        char[] strAry = source.ToArray();
+        for (int i = 0; i < strAry.Length; i++)
         {
-            var currentChar = sourceCharArray[i];
-            yield return UpperFirstCharacter(currentChar) +
-                         RepeatLowerCharacter(i, currentChar);
+            str += strAry[i].ToString().ToUpper();
+            for (int j = 0; j < i; j++)
+            {
+                str += strAry[i].ToString().ToLower();
+            }
+            str += "-";
         }
-    }
-
-    private static string UpperFirstCharacter(char currentChar)
-    {
-        return currentChar.ToString().ToUpper();
-    }
-
-    private static string RepeatLowerCharacter(int times, char currentChar)
-    {
-        return string.Concat(Enumerable.Repeat(currentChar.ToString().ToLower(), times));
+        return str.Substring(0,str.Length-1);
     }
 }
